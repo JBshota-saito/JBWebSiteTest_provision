3ea6cffa69bc448e16b70da41e9afab132b08d2e
JBshota-saito <65330926+JBshota-saito@users.noreply.github.com>
2020-05-28 16:15:04 +0900

    Update send_mail.php

--- send_mail.php
+++ send_mail.php
@@ -1,37 +1,59 @@
 <?php
+ include_once "./vendor/autoload.php";
+ /*
+  * Create the body of the message (a plain-text and an HTML version).
+  * $text is your plain-text email
+  * $html is your html version of the email
+  * If the receiver is able to view html emails then only the html
+  * email will be displayed
+  */
+ $text = "Hi!\nHow are you?\n";
+ $html = "<html>
+       <head></head>
+       <body>
+           <p>Hi!<br>
+               How are you?<br>
+           </p>
+       </body>
+       </html>";
+ // This is your From email address
+ $from = array('elsartshe@yahoo.co.jp' => 'Name To Appear');
+ // Email recipients
+ $to = array(
+       'jm0600909@gmail.com'=>'Destination 1 Name',
+       'elsartshe@yahoo.co.jp'=>'Destination 2 Name'
+ );
+ // Email subject
+ $subject = 'Example PHP Email';
 
- $url = 'https://api.sendgrid.com/';
- $user = 'test saito';
- $pass = 'P@ssw0rd';
+ // Login credentials
+ $username = 'testsaito';
+ $password = 'P@ssw0rd';
 
- $params = array(
-      'api_user' => $user,
-      'api_key' => $pass,
-      'to' => 'elsartshe@yahoo.co.jp',
-      'subject' => 'testing from curl',
-      'html' => 'testing body',
-      'text' => 'testing body',
-      'from' => 'elsartshe@yahoo.co.jp',
-   );
+ // Setup Swift mailer parameters
+ $transport = Swift_SmtpTransport::newInstance('smtp.sendgrid.net', 587);
+ $transport->setUsername($username);
+ $transport->setPassword($password);
+ $swift = Swift_Mailer::newInstance($transport);
 
- $request = $url.'api/mail.send.json';
+ // Create a message (subject)
+ $message = new Swift_Message($subject);
 
- // Generate curl request
- $session = curl_init($request);
+ // attach the body of the email
+ $message->setFrom($from);
+ $message->setBody($html, 'text/html');
+ $message->setTo($to);
+ $message->addPart($text, 'text/plain');
 
- // Tell curl to use HTTP POST
- curl_setopt ($session, CURLOPT_POST, true);
-
- // Tell curl that this is the body of the POST
- curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
-
- // Tell curl not to return headers, but do return the response
- curl_setopt($session, CURLOPT_HEADER, false);
- curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
-
- // obtain response
- $response = curl_exec($session);
- curl_close($session);
-
- // print everything out
- print_r($response);
+ // send message
+ if ($recipients = $swift->send($message, $failures))
+ {
+     // This will let us know how many users received this message
+     echo 'Message sent out to '.$recipients.' users';
+ }
+ // something went wrong =(
+ else
+ {
+     echo "Something went wrong - ";
+     print_r($failures);
+ }
